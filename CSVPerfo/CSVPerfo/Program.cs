7221637335771568b3977109aec69ca59a4305bb
Pierre Sid-idris <pierre.sididris@live.fr>
2020-10-27 12:26:35 +0100

    add C# project

---
+++ CSVPerfo/CSVPerfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSVPerfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var reader = new StreamReader(@"..\..\..\..\Dataset\cars.csv"))
+  
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    Console.WriteLine(line);
+
+                    //listA.Add(values[0]);
+                    //listB.Add(values[1]);
+                }
+            }
+        }
+    }
+}
